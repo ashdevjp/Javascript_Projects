ad8135123a188f575684a862481ed7750899ffb8
ashrizz <ashishjpaul@gmail.com>
2025-04-07 19:58:58 +0530

    generate HTML for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export const cart = [];
+export const cart = [{
+   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+   quantity: 2
+},{
+   productId:'8c9c52b5-5a19-4bcb-a5d1-158a74287c53',
+   quantity: 1
+}];
 
 export function addToCart(productId, quantity) {
    let matchingItem;
