905e60e08fcf6bacb750fc92191bb44975e2a943
ashdevjp <ashishjpaul@gmail.com>
2025-04-26 19:26:09 +0530

    save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -3,10 +3,12 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 if (!cart) {
    cart = [{
       productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-      quantity: 2
+      quantity: 2,
+      deliveryOptionId: '1'
    },{
       productId:'8c9c52b5-5a19-4bcb-a5d1-158a74287c53',
-      quantity: 1
+      quantity: 1,
+      deliveryOptionId: '2'
    }];
    
 
@@ -34,8 +36,9 @@ export function addToCart(productId, quantity) {
    } else {
       cart.push({
          productId: productId,
-         quantity: quantity
-      });
+         quantity: quantity,
+         deliveryOptionId: '1'
+      }); 
    }
 
    saveToStorage();
